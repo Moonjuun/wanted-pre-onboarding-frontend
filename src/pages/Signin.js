c3deef5559d939251f1b88537055c892e21c721b
Jun <cmoonjun11@gmail.com>
2023-04-01 19:50:36 +0900

    로그인 페이지 구현

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,10 +1,83 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Signin = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://pre-onboarding-selection-task.shop/auth/signin",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: state.email,
+            password: state.password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("jwt", data.token);
+        navigate("/todo");
+      } else {
+        alert("로그인 실패");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("로그인 실패");
+    }
+  };
+
   return (
-    <div>
-      <h1>이 곳은 로그인 화면입니다</h1>
-      <input data-testid="email-input" placeholder="이메일" />
-      <input data-testid="password-input" placeholder="비밀번호" />
-      <button data-testid="signup-button">회원가입</button>
+    <div className="Login">
+      <h1>로그인</h1>
+      <form onSubmit={handleSubmit}>
+        <section>
+          <div>이메일</div>
+          <input
+            name="email"
+            value={state.email}
+            data-testid="email-input"
+            className="Login_email"
+            placeholder="이메일"
+            onChange={handleChangeState}
+          />
+        </section>
+        <section>
+          <div>비밀번호</div>
+          <input
+            name="password"
+            value={state.password}
+            data-testid="password-input"
+            className="Login_password"
+            placeholder="비밀번호"
+            type="password"
+            onChange={handleChangeState}
+          />
+        </section>
+        <button data-testid="login-button" type="submit">
+          로그인
+        </button>
+      </form>
     </div>
   );
 };
